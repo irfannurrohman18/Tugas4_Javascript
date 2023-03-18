e528aaa4b670354e706da59a637ca8732cfdcba9
irfannurrohman18 <putraremaja181@gmail.com>
2023-03-18 09:05:39 +0700

    Menambahkan file form login dengan fungsi javascript

---
+++ js/skripku.js
@@ -0,0 +1,12 @@
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+  
+    if (username === "mimin" && password === "mimin123") {
+      alert("Selamat Datang Mimin, Anda Administrator");
+    } else if (username === "budi" && password === "budi123") {
+      alert("Selamat Datang Budi, Anda Operator");
+    } else {
+      alert("Maaf Anda Gagal Login");
+    }
+  }
